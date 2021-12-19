34ac1f4440e8350d66301982e470845a3f25acf9
BolajiOlajide <25608335+BolajiOlajide@users.noreply.github.com>
2021-12-19 04:42:52 +0100

    feat: add debug stmt when reporting perf

--- frontend/src/reportWebVitals.js
+++ frontend/src/reportWebVitals.js
@@ -1,5 +1,6 @@
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
+    console.log('reporting pef')
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
