d4e6e4066edd523762fb57c9e37f814253e735c3
BolajiOlajide <25608335+BolajiOlajide@users.noreply.github.com>
2021-12-19 04:36:24 +0100

    feat: complete dockerizing for frontend

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -6,3 +6,9 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders p tag for editing', () => {
+  render(<App />);
+  const paragraphElement = screen.getByText(/do not edit/i);
+  expect(paragraphElement).toBeInTheDocument();
+});
